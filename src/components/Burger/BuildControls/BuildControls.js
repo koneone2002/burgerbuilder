a4f8ce406aeb94a6494db8931ab93fe5e6ebbac6
koneone2002 <katie@imagings-MacBook-Pro.local>
2018-10-15 18:27:11 -0700

    burgerbuilder project

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Meat', type: 'meat'},
+];
+
+const buildControls = (props) => (
+  <div className="BuildControls">
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+    {controls.map(ctrl => (
+      <BuildControl key={ctrl.label} 
+      label={ctrl.label}
+      removed={() => props.ingredientRemoved(ctrl.type)}
+      added={() => props.ingredientAdded(ctrl.type)}
+      disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+    <button className="OrderButton"
+    disabled={!props.purchaseable}>ORDER NOW</button>
+  </div>
+);
+
+
+
+
+export default buildControls;
